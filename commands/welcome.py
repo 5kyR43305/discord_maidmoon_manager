3b68b4381b60a8825bb23b5ab532000b15f0c62d
5kyR43305 <5kyr43305@gmail.com>
2025-09-04 20:40:22 +0900

    welcomed men max5_mk2

--- commands/welcome.py
+++ commands/welcome.py
@@ -21,13 +21,11 @@ class Welcome(commands.Cog):
             await ctx.send("❗멘션은 최대 5명까지 가능합니다.")
             return
 
-        # 멘션 문자열 생성
-        member_mentions = ' '.join([member.mention for member in members])
-        member_names = ' '.join([member.display_name for member in members])
+        # 각 멤버 멘션에 '님'을 붙여서 문자열 생성
+        member_mentions = ' '.join([f'{member.mention}님' for member in members])
 
         welcome_message = (
-            f'# <a:g1:1381626468735385600> {member_mentions} 님 환영합니다!\n'
-            f'<a:s10:1381626541150175332> {member_names} 님 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
+            f'{member_mentions} 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
             f'<a:s10:1381626541150175332> <#1381621263730086060>에서 규칙을 꼭 확인해주세요!\n'
             f'<:19_:1381626681357238452> 규칙을 읽지 않아 생기는 불이익은 책임지지 않아요!\n\n'
             f'<a:s10:1381626541150175332> 적응이 어렵다면 <@&1381621262291570842> 를 맨션해주세요!\n\n'
