a0625b4f008175a37a1443138ee6e4cbcc4f4fd8
5kyR43305 <5kyr43305@gmail.com>
2025-09-04 20:29:05 +0900

    welcomed men max5

--- commands/welcome.py
+++ commands/welcome.py
@@ -1,5 +1,3 @@
-# commands/welcome.py
-
 import discord
 from discord.ext import commands
 
@@ -8,13 +6,28 @@ class Welcome(commands.Cog):
         self.bot = bot
 
     @commands.command(name='환영')
-    async def welcome_prefix(self, ctx, member: discord.Member):
+    async def welcome_prefix(self, ctx, *members: discord.Member):
         """
         특정 멤버에게 환영 메시지를 보냅니다.
+        최대 5명까지 멘션 가능합니다.
         """
+        # 멘션된 멤버가 없는 경우
+        if not members:
+            await ctx.send("❗환영 메시지를 보낼 멤버를 멘션해주세요.")
+            return
+
+        # 멘션된 멤버가 5명을 초과하는 경우
+        if len(members) > 5:
+            await ctx.send("❗멘션은 최대 5명까지 가능합니다.")
+            return
+
+        # 멘션 문자열 생성
+        member_mentions = ' '.join([member.mention for member in members])
+        member_names = ' '.join([member.display_name for member in members])
+
         welcome_message = (
-            f'# <a:g1:1381626468735385600> <@{member.id}> 님 환영합니다!\n'
-            f'<a:s10:1381626541150175332> {member.mention} 님 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
+            f'# <a:g1:1381626468735385600> {member_mentions} 님 환영합니다!\n'
+            f'<a:s10:1381626541150175332> {member_names} 님 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
             f'<a:s10:1381626541150175332> <#1381621263730086060>에서 규칙을 꼭 확인해주세요!\n'
             f'<:19_:1381626681357238452> 규칙을 읽지 않아 생기는 불이익은 책임지지 않아요!\n\n'
             f'<a:s10:1381626541150175332> 적응이 어렵다면 <@&1381621262291570842> 를 맨션해주세요!\n\n'
