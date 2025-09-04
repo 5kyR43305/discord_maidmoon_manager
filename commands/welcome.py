8dbda1184e2d079a5bf86a64e89f77b3d22385f3
5kyR43305 <5kyr43305@gmail.com>
2025-09-04 20:46:53 +0900

    welcomed men max5 MK3

--- commands/welcome.py
+++ commands/welcome.py
@@ -21,16 +21,18 @@ class Welcome(commands.Cog):
             await ctx.send("❗멘션은 최대 5명까지 가능합니다.")
             return
 
-        # 각 멤버 멘션에 '님'을 붙여서 문자열 생성
-        member_mentions = ' '.join([f'{member.mention}님' for member in members])
-
+        # 각 멘션 뒤에 '님'을 붙여서 문자열 생성 (예: @5kyR4님 @rAI님 ...)
+        member_mentions_with_nim = ' '.join([f'{member.mention}님' for member in members])
+        
+        # 각 멤버의 표시 이름 뒤에 '님'을 붙여서 문자열 생성 (예: 5kyR4님 rAI님 ...)
+        member_names_with_nim = ' '.join([f'{member.display_name}님' for member in members])
+        
+        # 최종 환영 메시지 구성
         welcome_message = (
-            f'{member_mentions} 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
-            f'<a:s10:1381626541150175332> <#1381621263730086060>에서 규칙을 꼭 확인해주세요!\n'
-            f'<:19_:1381626681357238452> 규칙을 읽지 않아 생기는 불이익은 책임지지 않아요!\n\n'
-            f'<a:s10:1381626541150175332> 적응이 어렵다면 <@&1381621262291570842> 를 맨션해주세요!\n\n'
-            f'<:1911:1381626675489669220> 앞으로 잘 부탁드려요!\n'
-            f'<@&1381621262291570844>'
+            f'{member_mentions_with_nim} 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n'
+            f'{member_names_with_nim} 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다! ⁠𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍⁠︱୨📜୧₊：규칙⸝⸝에서 규칙을 꼭 확인해주세요!\n\n'
+            f'<:19:1381626681357238452> 규칙을 읽지 않아 생기는 불이익은 책임지지 않아요! 적응이 어렵다면 @알-수-없는-역할 를 맨션해주세요! 앞으로 잘 부탁드려요!\n'
+            f'@알-수-없는-역할'
         )
         await ctx.send(welcome_message)
 
